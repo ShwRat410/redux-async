0d75c5c4c0013f97489d2e71a9b60b6896cdbb19
ShwRat410 <shwetaratnawat410@gmail.com>
2025-03-13 16:27:50 +0000

    Using useEffect with redux for api calls

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
 
+
 function App() {
   const showCart = useSelector((state) => state.ui.showCart)
+  const cart = useSelector((state)=>state.cart)
+
+  useEffect(()=>{
+    fetch("https://react-project-71169-default-rtdb.firebaseio.com/cart.json",{
+      method:"PUT",
+      body:JSON.stringify(cart)
+    })
+  },[cart])
+
   return (
       <Layout>
         {showCart &&<Cart />}
